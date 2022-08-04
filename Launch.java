2ca8a5a2fa93aa6d8dbf2f8344a95bb8120bc5d3
Saiteja <104367656+Saiteja66466@users.noreply.github.com>
2022-08-04 17:47:39 +0530

    Add files via upload

---
+++ Launch.java
@@ -0,0 +1,17 @@
+public class Launch {
+    public static void main(String[] args)
+    {
+        int n = 5;
+		for(int i=1; i<n; i++)
+        {
+            for(int j=1; j<n; j++)
+            {
+                if(i==0)
+                System.out.print(j);
+                else
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
